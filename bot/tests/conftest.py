e6535edee5e671fcc306a3a7f5b994213e4266fe
Victor Patrin <victor.patrin@protonmail.com>
2026-02-19 17:22:07 -0500

    needs default for tests

--- bot/tests/conftest.py
+++ bot/tests/conftest.py
@@ -1,4 +1,5 @@
 import os
 
-# Set before any bot module is imported (BotSettings requires TELEGRAM_BOT_TOKEN).
+# Set before any bot module is imported (BotSettings requires these).
 os.environ.setdefault("TELEGRAM_BOT_TOKEN", "test-token")
+os.environ.setdefault("NOTIFICATION_POLL_INTERVAL", "60")
