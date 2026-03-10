c2cec8aa9e4bdf19f05aac4c7ce76b437e85770c
Victor Patrin <victor.patrin@protonmail.com>
2026-03-10 19:52:10 -0400

    dark mode boiler plate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 function App() {
   return (
-    <div className="min-h-screen bg-black text-orange-500 flex items-center justify-center">
-      <h1 className="text-4xl font-mono font-bold">Bonjour,Victor</h1>
+    <div className="min-h-screen bg-background text-foreground flex flex-col items-center justify-center gap-6">
+      <h1 className="text-4xl font-mono font-bold">Bonjour, Victor</h1>
+      <Button>Test Button</Button>
     </div>
   )
 }
