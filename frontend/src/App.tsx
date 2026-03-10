40eea3a6f087967998a2b9fca3583fd6ab2b8928
Victor Patrin <victor.patrin@protonmail.com>
2026-03-10 19:48:39 -0400

    scaffolding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen bg-black text-orange-500 flex items-center justify-center">
-      <h1 className="text-4xl font-mono font-bold">SAQ Sommelier</h1>
+      <h1 className="text-4xl font-mono font-bold">Bonjour,Victor</h1>
     </div>
   )
 }
